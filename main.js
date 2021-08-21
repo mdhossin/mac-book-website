b084b8a2057604b9e7d08081423ba8275ea7a163
md sahadat hossin <mdsahadathossin778@gmail.com>
2021-08-21 13:56:32 +0600

    solve some issues

--- main.js
+++ main.js
@@ -1,75 +1,89 @@
 // get all extra cost id
-const memoryCost = document.getElementById("memoryCost");
-const storageCost = document.getElementById("storageCost");
-const deliveryCost = document.getElementById("deliveryCost");
+const memoryId = document.getElementById("memoryCost");
+const storageId = document.getElementById("storageCost");
+const deliveryId = document.getElementById("deliveryCost");
 
-// update bonus total price
-const bonusTotal = document.getElementById("totalCost");
+// get all total price id
 
-function totalPrice(costId) {
-  const extraCostId = document.getElementById(costId + "Cost");
-  const extraCost = parseInt(extraCostId.innerText);
-  const totalCostAmount = parseInt(bonusTotal.innerText);
-  bonusTotal.innerText = totalCostAmount + extraCost;
-}
+const bestPrice = document.getElementById("bestPrice");
+const totalPrice = document.getElementById("totalPrice");
+const grandTotalPrice = document.getElementById("grandTotal");
+
 //  update total price
 
 function totalCostPrice(cost) {
   const extraCostId = document.getElementById(cost + "Cost");
   const extraCost = parseInt(extraCostId.innerText);
-  const totalPrice = document.getElementById("totalPrice");
-  const totalAmount = parseInt(totalPrice.innerText);
+  const bonusTotal = document.getElementById("bestPrice");
+  const totalAmount = parseInt(bonusTotal.innerText);
   totalPrice.innerText = totalAmount + extraCost;
+}
+
+// update total price cost
+
+function updateTotal() {
+  const bestPriceCost = parseInt(bestPrice.innerText);
+  const memoryCost = parseInt(memoryId.innerText);
+  const storageCost = parseInt(storageId.innerText);
+  const deliveryCost = parseInt(deliveryId.innerText);
+  const totalAmount = bestPriceCost + memoryCost + storageCost + deliveryCost;
+  totalPrice.innerText = totalAmount;
+  const grandTotal = document.getElementById("grandTotal");
+  grandTotal.innerText = totalAmount;
 }
 
 // add event listener memory button
 document.getElementById("memorySmall").addEventListener("click", function () {
-  memoryCost.innerText = "0";
+  memoryId.innerText = "0";
+  totalCostPrice("memory");
+  updateTotal();
 });
 document.getElementById("memoryBig").addEventListener("click", function () {
-  memoryCost.innerText = "180";
+  memoryId.innerText = "180";
   totalCostPrice("memory");
-  totalPrice("memory");
+  updateTotal();
 });
 
 // add event listener storage button
 document.getElementById("storageSmall").addEventListener("click", function () {
-  storageCost.innerText = "0";
+  storageId.innerText = "0";
+  totalCostPrice("storage");
+  updateTotal();
 });
 
 document.getElementById("storageMedium").addEventListener("click", function () {
-  storageCost.innerText = "100";
+  storageId.innerText = "100";
   totalCostPrice("storage");
-  totalPrice("storage");
+  updateTotal();
 });
 
 document.getElementById("storageBig").addEventListener("click", function () {
-  storageCost.innerText = "180";
+  storageId.innerText = "180";
   totalCostPrice("storage");
-  totalPrice("storage");
+  updateTotal();
 });
 
 // add event listener delivery button
 document.getElementById("freeDelivery").addEventListener("click", function () {
-  deliveryCost.innerText = "0";
+  deliveryId.innerText = "0";
+  totalCostPrice("delivery");
+  updateTotal();
 });
 document
   .getElementById("deliveryCharge")
   .addEventListener("click", function () {
-    deliveryCost.innerText = "20";
+    deliveryId.innerText = "20";
     totalCostPrice("delivery");
-    totalPrice("delivery");
+    updateTotal();
   });
-
-// update total discount
 
 function promoDiscount() {
   const promoInput = document.getElementById("promoInput");
   const promoValue = promoInput.value;
   const totalPrice = document.getElementById("totalPrice");
-  const totalAmountCost = parseInt(totalPrice.innerText);
+  const totalCost = parseInt(totalPrice.innerText);
   if (promoValue == "stevekaku") {
-    bonusTotal.innerText = totalAmountCost - (totalAmountCost * 20) / 100;
+    grandTotalPrice.innerText = totalCost - (totalCost * 20) / 100;
     document.getElementById("promoButton").disabled = true;
     promoInput.value = "";
   } else {
